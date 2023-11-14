0b5658b5265283900bb87bdd29bdc3406ccbf54e
M0rl0ck <seriy17@yandex.ru>
2023-11-14 21:39:46 +0300

    feat: add reload button and refactor fetch data

--- hacker-news/src/pages/main/mainPage.tsx
+++ hacker-news/src/pages/main/mainPage.tsx
@@ -1,24 +1,48 @@
 import { useEffect, useState } from "react";
-import { useGetNewsQuery, useLazyGetItemQuery } from "../../store/API/HN_API";
+import {
+  useLazyGetNewsQuery,
+  useLazyGetItemQuery,
+} from "../../store/API/HN_API";
 import type { INews } from "../../infostructure/INews";
 import NewsShort from "../../components/News/News";
+import styles from "./mainPage.module.css";
+
+enum NEWS {
+  LENGTH_SLICE_NEWS = 10,
+  AMOUN_NEWS = 100,
+}
 
 const MainPage = () => {
-  const { data, isFetching } = useGetNewsQuery();
+  const [isLoading, setIsloading] = useState(false);
   const [getItem] = useLazyGetItemQuery();
   const [news, setNews] = useState<INews[]>([]);
+  const [getNewsList, { data, isFetching }] = useLazyGetNewsQuery();
 
   useEffect(() => {
+    getNewsList();
+  }, [getNewsList]);
+
+  useEffect(() => {
+    async function getSliceNews(list: string[]) {
+      const stories = [];
+      for (let i = 0; i < list.length && i < NEWS.LENGTH_SLICE_NEWS; i++) {
+        stories.push(getItem(list[i]));
+      }
+      const promisesStories = await Promise.all(stories);
+      setNews((prev) => [
+        ...prev,
+        ...promisesStories.map((el) => el.data as INews),
+      ]);
+      setIsloading(false);
+    }
+
     async function getNews(data: string[]) {
-      let tempNews = 0;
+      setIsloading(true);
+      const listStoriesId = [...data];
       let index = 0;
-      while (tempNews <= 100 && index < data.length) {
-        const { data: a } = await getItem(data[index]);
-        if (a && a.type === "story") {
-          tempNews++;
-          setNews((prev) => [...prev, a]);
-        }
-        index++;
+      while (index < listStoriesId.length && index < NEWS.AMOUN_NEWS) {
+        getSliceNews(listStoriesId.slice(index));
+        index += NEWS.LENGTH_SLICE_NEWS;
       }
     }
     if (data) {
@@ -28,12 +52,23 @@ const MainPage = () => {
   }, [data, getItem]);
 
   return (
-    <div>
-      {isFetching && <h1>Loading...</h1>}
-      {news &&
-        !!news.length &&
-        news.map((el) => <NewsShort key={el.id} news={el} />)}
-    </div>
+    <section>
+      <div>
+        {(isFetching || isLoading) && <h1>Loading...</h1>}
+        {news &&
+          !!news.length &&
+          news.map((el) => <NewsShort key={el.id} news={el} />)}
+      </div>
+      <div className={styles.buttonContainer}>
+        <button
+          onClick={() => {
+            getNewsList();
+          }}
+        >
+          Reload
+        </button>
+      </div>
+    </section>
   );
 };
 
